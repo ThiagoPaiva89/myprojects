e62e4197beb67b402e29c250e3d2d75b09090733
Thiago Paiva <thiagoinaciopaiva@hotmail.com>
2018-02-26 09:27:26 -0300

    teste projeto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+	<head>
+		<meta charset="utf-8"/>
+		<title>Exemplo 1 PHP </title>
+	</head>
+	
+	<body>
+		<h1>Testando PHP</h1>
+		<?php
+			echo "<h2> Olá, </br> mundo!</h2>";
+		?>
+	</body>
+</html>	
